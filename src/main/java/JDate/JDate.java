ad82b36ede82430f00300db6325256cc29662318
v0ncent <83373495+v0ncent@users.noreply.github.com>
2023-05-07 23:59:25 -0500

    Started creating main JDate Object

--- src/main/java/JDate/JDate.java
+++ src/main/java/JDate/JDate.java
@@ -1,4 +1,52 @@
+// JDate Main Class
+// @Auth v0ncent
 package JDate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+
 public class JDate {
+    /** Instance of logger for JDate class*/
+    private static final Logger LOGGER = LoggerFactory.getLogger(JDate.class);
+
+    private final String name;
+
+    private final int exitOperation;
+
+    public JDate(String name) {
+        this(name,JFrame.EXIT_ON_CLOSE);
+    }
+
+    public JDate(String name, int exitOperation) {
+        this.name = name;
+        this.exitOperation = exitOperation;
+
+        JFrame frame = new JFrame(name);
+        frame.setDefaultCloseOperation(exitOperation);
+    }
+
+    public void runGame() {
+        LOGGER.info(
+                "\n     _____       __     \n" +
+                        " __ / / _ \\___ _/ /____ \n" +
+                        "/ // / // / _ `/ __/ -_)\n" +
+                        "\\___/____/\\_,_/\\__/\\__/ \n" +
+                        "                        "
+        );
+
+
+
+    }
+
+    // -GETTERS / SETTERS-
+    public String getName() {
+        return this.name;
+    }
+
+    public int getExitOperation() {
+        return this.exitOperation;
+    }
+
 }
--- src/main/java/Tests.java
+++ src/main/java/Tests.java
@@ -1,7 +1,38 @@
+import JDate.JDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Tests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Tests.class);
+
+    private static void testConstructors() {
+        LOGGER.info("Testing Constructor JDate(String name, int windowWidth, int windowHeight);");
+
+        try {
+            new JDate("Constructor JDate(String name, int windowWidth, int windowHeight);");
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+
+    }
+
+    private static void testRun() {
+        LOGGER.info("Testing jDate.runGame();");
+
+        JDate testRun = new JDate("Testing jDate.runGame();");
+
+        try {
+            testRun.runGame();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        LOGGER.info("===Running Tests===");
+
+        testConstructors();
+        testRun();
     }
 
 }
