202100ba32796f454218ee4cc1d540a8c08d2e82
v0ncent <83373495+v0ncent@users.noreply.github.com>
2023-05-11 19:55:46 -0500

    started creating character class

--- src/main/java/JDate/Character.java
+++ src/main/java/JDate/Character.java
@@ -1,7 +1,29 @@
+// JDate Character Class
+// @Auth v0ncent
 package JDate;
 
-public class Character {
-    public Character(String name) {
+import java.util.ArrayList;
+import java.util.Objects;
 
+public class Character {
+    private final String name;
+
+    public final ArrayList<String> aliases;
+
+    public Character(String name) {
+        this(name, null);
+    }
+
+    public Character(String name, ArrayList<String> aliases) {
+        this.name = name;
+        this.aliases = Objects.requireNonNullElseGet(aliases, ArrayList::new);
+    }
+
+    // Getters / Setters
+    public String getName() {
+        return this.name;
+    }
+    public ArrayList<String> getAliases() {
+        return this.aliases;
     }
 }
